f2239a8fc41d6baf71e84dde5d2c83e4d0e89c26
Amir Bravostack <chukwuebukaonuh23@gmail.com>
2025-08-17 07:55:42 +0100

    Enhance documentation in doc.txt and update main.c to demonstrate usage of %d and %f format specifiers

--- main.c
+++ main.c
@@ -3,13 +3,15 @@
 int main() {
     
     // check
-    int age = 32;
-    int year = 1447;
-    int quantity = 7;
+    float gpa = 3.76;
+    float price = 500.99;
+    float temperature = -6.5;
+    double pi = 3.14159265358979;
 
-    printf("You are %d\n", age);
-    printf("The year is %d\n", year);
-    printf("You have purchase our products: %d times", quantity);
+    printf("Your gpa is: %.2f\n", gpa);
+    printf("The item you bought would cost you: N%.2f\n", price);
+    printf("It is currently %.1fC degrees outside\n", temperature);
+    printf("The of pi is %.14lf", pi);
 
     return 0;
 }
