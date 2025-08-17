b5bd1eeb806a8b06c9b7fba77b31e0b8317272dd
Amir Bravostack <chukwuebukaonuh23@gmail.com>
2025-08-17 08:28:59 +0100

    Fix greeting output format and add food description in main.c

--- main.c
+++ main.c
@@ -4,8 +4,10 @@ int main() {
     
     // check
     char greeting[] = "Marhaban";
+    char food[] = "Jollof Rice";
 
-    printf("%s, ya habibi: ", greeting);
+    printf("%s, ya habibi: \n", greeting);
+    printf("I suppose we all fancy a %s\n", food);
 
     return 0;
 }
