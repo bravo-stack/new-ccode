48c22da64c0c9a35390a576c04be0cf8b69b2e00
Amir Bravostack <chukwuebukaonuh23@gmail.com>
2025-08-17 08:18:02 +0100

    Refactor main.c to remove unused variables and simplify output to display grade

--- main.c
+++ main.c
@@ -3,15 +3,10 @@
 int main() {
     
     // check
-    float gpa = 3.76;
-    float price = 500.99;
-    float temperature = -6.5;
-    double pi = 3.14159265358979;
+    char grade = 'A';
+    char value_sign = 'N';
 
-    printf("Your gpa is: %.2f\n", gpa);
-    printf("The item you bought would cost you: N%.2f\n", price);
-    printf("It is currently %.1fC degrees outside\n", temperature);
-    printf("The of pi is %.14lf", pi);
+    printf("here is your grade: %c", grade);
 
     return 0;
 }
