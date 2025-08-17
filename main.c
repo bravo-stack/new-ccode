e21b8d7b20bfc1e3d38006b6c6cc09fd51a6eba8
Amir Bravostack <chukwuebukaonuh23@gmail.com>
2025-08-17 07:37:15 +0100

    Add .gitignore, documentation, and main program files

---
+++ main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    
+    // check
+    int age = 32;
+    int year = 1447;
+    int quantity = 7;
+
+    printf("You are %d\n", age);
+    printf("The year is %d\n", year);
+    printf("You have purchase our products: %d times", quantity);
+
+    return 0;
+}
